3440d3914721a7e942d87cbc4289694debb3eb8c
unknown <mutungaeric839@gmail.com>
2023-06-12 16:20:55 +0300

    contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,59 +7,31 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return Contact::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $contact = Contact::create($request->all());
+        return response()->json($contact, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Contact $contact)
     {
-        //
+        return $contact;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Contact $contact)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Contact $contact)
     {
-        //
+        $contact->update($request->all());
+        return response()->json($contact, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Contact $contact)
+    public function destroy(Contact $report)
     {
-        //
+        $report->delete();
+        return response()->json(null, 204);
     }
 }

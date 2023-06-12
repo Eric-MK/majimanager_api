f40fe8a3690d10fc6caee5905b0d5ea296ca7dc8
unknown <mutungaeric839@gmail.com>
2023-06-12 20:42:09 +0300

    kernel,checkadmin,apiphp

--- routes/api.php
+++ routes/api.php
@@ -8,17 +8,31 @@ use App\Http\Controllers\TipController;
 use App\Http\Controllers\AuthController;
 
 
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->group( function () {
+
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Common User routes
+    Route::get('reports', [ReportController::class, 'index']);
+    Route::get('reports/{id}', [ReportController::class, 'show']);
+    Route::post('reports', [ReportController::class, 'store']); // allow users to create reports
+
+    Route::get('contacts', [ContactController::class, 'index']);
+    Route::get('contacts/{id}', [ContactController::class, 'show']);
+
+    Route::get('tips', [TipController::class, 'index']);
+    Route::get('tips/{id}', [TipController::class, 'show']);
+
+    Route::middleware(['admin'])->group(function () {
+        // Admin routes
+        Route::apiResource('reports', ReportController::class)->except(['index', 'show', 'store']); // except store because it's defined above
+        Route::apiResource('contacts', ContactController::class);
+        Route::apiResource('tips', TipController::class);
+    });
 });
 
-
-Route::apiResource('reports', ReportController::class);
-Route::apiResource('contacts', ContactController::class);
-Route::apiResource('tips', TipController::class);
 
 
 

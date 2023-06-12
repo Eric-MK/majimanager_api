92077658248d1f002e750f244c9794cca06c1b87
unknown <mutungaeric839@gmail.com>
2023-06-12 15:50:39 +0300

    report relationship with user

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    protected $fillable = [//fillable property in Laravel is used to define which attributes of a model can be filled when creating or updating a model instance.
+        'user_id',
+        'location',
+        'description',
+        'status',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

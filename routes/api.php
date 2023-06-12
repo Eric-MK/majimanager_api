8b66d9c22e49d1811cbfbf9e2d7e2b07b0ab6330
unknown <mutungaeric839@gmail.com>
2023-06-12 17:33:52 +0300

    authentication routes

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\TipController;
+use App\Http\Controllers\AuthController;
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 Route::apiResource('reports', ReportController::class);
 Route::apiResource('contacts', ContactController::class);
 Route::apiResource('tips', TipController::class);
+
+
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

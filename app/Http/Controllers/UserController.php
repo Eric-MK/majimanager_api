5131bd71cbca59149b88467c940ecaa90ae06ede
unknown <mutungaeric839@gmail.com>
2023-06-25 17:15:40 +0300

    adding user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User; // Assuming User is your model class
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    // Show all users
+    public function index()
+    {
+        return User::all();
+    }
+
+    // Show a single user
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    // Create a new user
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ]);
+    }
+
+    // Update a user
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$id,
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'message' => 'User updated successfully',
+            'user' => $user
+        ]);
+    }
+
+    // Delete a user
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully'
+        ]);
+    }
+}
